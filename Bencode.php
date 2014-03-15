5094826bfa99ecca88fd41f495bda548d2dde310
Jesse Schalken <me@jesseschalken.com>
2014-03-15 13:09:23 +1100

    Bencode encoder/decoder complete. Passing tests with 100% coverage included.

---
+++ Bencode.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace EasyBencode;
+
+class Bencode {
+    /**
+     * @param int|string|array $value
+     * @throws Exception
+     * @return string
+     */
+    static function encode($value) {
+        if (is_array($value)) {
+            if (self::isAssoc($value)) {
+                $result = '';
+
+                foreach ($value as $k => $v)
+                    $result .= self::encode("$k") . self::encode($v);
+
+                return "d{$result}e";
+            } else {
+                $result = '';
+
+                foreach ($value as $v)
+                    $result .= self::encode($v);
+
+                return "l{$result}e";
+            }
+        } else if (is_int($value)) {
+            return "i{$value}e";
+        } else if (is_string($value)) {
+            return strlen($value) . ":$value";
+        } else {
+            $type = gettype($value);
+
+            throw new Exception("Bencode supports only integers, strings and arrays. $type given.");
+        }
+    }
+
+    private static function isAssoc(array $array) {
+        $i = 0;
+
+        foreach ($array as $k => $v)
+            if ($k !== $i++)
+                return true;
+
+        return false;
+    }
+
+    /**
+     * @param string $string Bencode string
+     * @throws Exception
+     * @return int|string|array
+     */
+    static function decode($string) {
+        $parser = new BencodeParser($string);
+        return $parser->decode();
+    }
+}
+
+class BencodeParser {
+    private $string, $pos = 0;
+
+    function __construct($string) {
+        $this->string = $string;
+    }
+
+    function decode() {
+        switch ($this->read()) {
+            case 'i':
+                $this->seek();
+                return $this->readInt('e');
+            case 'l':
+                $this->seek();
+                $result = array();
+
+                while ($this->read() !== 'e')
+                    $result[] = $this->decode();
+
+                $this->seek();
+                return $result;
+            case 'd':
+                $this->seek();
+                $result = array();
+
+                while ($this->read() !== 'e')
+                    $result[$this->decode()] = $this->decode();
+
+                $this->seek();
+                return $result;
+            default:
+                $len = $this->readInt(':');
+                if ($len < 0)
+                    throw new Exception("Length of string ($len) must not be negative");
+                return $this->remove($len);
+        }
+    }
+
+    private function read($len = 1) {
+        $result = (string)substr($this->string, $this->pos, $len);
+
+        $len2 = strlen($result);
+        if ($len !== $len2)
+            throw new Exception("$len bytes expected but only $len2 bytes remain");
+
+        return $result;
+    }
+
+    private function seek($len = 1) {
+        $this->pos += $len;
+    }
+
+    private function readInt($delimiter) {
+        $result = $this->remove($this->find($delimiter));
+        $this->seek(strlen($delimiter));
+        $int = (int)$result;
+        if ("$int" !== $result)
+            throw new Exception("Invalid integer: $result");
+        return $int;
+    }
+
+    private function remove($len) {
+        $string = $this->read($len);
+        $this->seek($len);
+        return $string;
+    }
+
+    private function find($needle) {
+        $pos = strpos($this->string, $needle, $this->pos);
+        if ($pos === false)
+            throw new Exception("'$needle' not found");
+        return $pos - $this->pos;
+    }
+}
+
+class Exception extends \Exception {
+}
---
+++ BencodeTest.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace EasyBencode;
+
+class BencodeTest extends \PHPUnit_Framework_TestCase {
+    function testNumbers() {
+        self::assertBencode(0, 'i0e');
+        self::assertBencode(34957683495, 'i34957683495e');
+        self::assertBencode(-87, 'i-87e');
+        self::assertBencode('123456789012', '12:123456789012');
+    }
+
+    static function assertBencode($value, $bencode) {
+        self::assertEquals($bencode, Bencode::encode($value));
+        self::assertEquals($value, Bencode::decode($bencode));
+    }
+
+    function testTorrentFile() {
+        $bencode = <<<'s'
+d8:announce39:http://torrent.foobar.baz:9374/announce13:announce-listll39:http://torrent.foobar.baz:9374/announceel44:http://ipv6.torrent.foobar.baz:9374/announceee7:comment31:My torrent comment goes here :)13:creation datei1382003607e4:infod6:lengthi925892608e4:name13:some-file.boo12:piece lengthi524288e6:pieces0:ee
+s;
+        $torrent = array(
+            'announce' => 'http://torrent.foobar.baz:9374/announce',
+            'announce-list' => array(
+                array('http://torrent.foobar.baz:9374/announce'),
+                array('http://ipv6.torrent.foobar.baz:9374/announce'),
+            ),
+            'comment' => 'My torrent comment goes here :)',
+            'creation date' => 1382003607,
+            'info' => array(
+                'length' => 925892608,
+                'name' => 'some-file.boo',
+                'piece length' => 524288,
+                'pieces' => '',
+            ),
+        );
+
+        self::assertBencode($torrent, $bencode);
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Invalid integer: -0
+     */
+    function testNegativeZero() {
+        Bencode::decode('i-0e');
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Length of string (-9) must not be negative
+     */
+    function testNegativeStringLength() {
+        Bencode::decode("-9:string");
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Invalid integer: 8f
+     */
+    function testInvalidInteger() {
+        Bencode::decode('i8fe');
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage 1 bytes expected but only 0 bytes remain
+     */
+    function testMissingE() {
+        Bencode::decode('d2:ffi8e');
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage 8 bytes expected but only 3 bytes remain
+     */
+    function testStringLengthMismatch() {
+        Bencode::decode('8:foo');
+    }
+
+    function testList() {
+        self::assertBencode(array(9, 2, 4, 'foo', 'bar', array()), 'li9ei2ei4e3:foo3:barlee');
+    }
+
+    function testDictionary() {
+        self::assertBencode(array('foo' => 'bar', 'baz' => 'boo'), 'd3:foo3:bar3:baz3:booe');
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage ':' not found
+     */
+    function testOddDictionaryMembers() {
+        Bencode::decode('di0ee');
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Bencode supports only integers, strings and arrays. double given.
+     */
+    function testFloat() {
+        Bencode::encode(0.0);
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Bencode supports only integers, strings and arrays. object given.
+     */
+    function testObject() {
+        Bencode::encode(new \stdClass);
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Bencode supports only integers, strings and arrays. NULL given.
+     */
+    function testNull() {
+        Bencode::encode(null);
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Bencode supports only integers, strings and arrays. boolean given.
+     */
+    function testBool() {
+        Bencode::encode(true);
+    }
+}
+ 
